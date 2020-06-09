d42f5333f8e33595130fcfc5f574133dee3db0b9
Jeff Cook <jecook2692@gmail.com>
2020-06-09 17:16:46 -0400

    adds constructor, click handler and render function to flip cards and changes MemoryCard_inner class to innerClass

--- memory-game/src/components/MemoryCard.jsx
+++ memory-game/src/components/MemoryCard.jsx
@@ -3,19 +3,27 @@ import './MemoryCard.css';
 
 
 class MemoryCard_back extends Component {
-    constructor(){
-        super();
+    constructor(props){
+        super(props);
         this.state = {isFlipped: false};
-
     }
     
     clickHandler(){
-       this.setState({isFlipped: !this.state.isFlipped});
+        this.setState ({
+            isFlipped: !this.state.isFlipped
+        });
     }
+    
     render() {
+        let innerClass = "MemoryCard_inner"
+        
+        if(this.state.isFlipped) {
+           innerClass = "MemoryCard_inner flipped";
+        }
+        
         return (
-            <div className="MemoryCard" onClick={(e) => this.clickHandler(e)}>
-                <div className="MemoryCard_inner">
+            <div className="MemoryCard" onClick={this.clickHandler.bind(this)}>
+                <div className={innerClass}>
                     <div className="MemoryCard_back">
                         <img src="https://www.digitalcrafts.com/img/logo-wrench-white.png" alt="img of wrench"></img>
                     </div>
