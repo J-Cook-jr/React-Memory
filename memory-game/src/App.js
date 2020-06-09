758f627b8800e37fc9d7289c59b3c69c575c3f2a
Jeff Cook <jecook2692@gmail.com>
2020-06-09 11:29:44 -0400

    creates react app, changes the title and subtitle in App.js and changes the app.header height in App.css

---
+++ memory-game/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Memory Game</h1>
+        <h4>Match Cards to win</h4>
+      </header>
+    </div>
+  );
+}
+
+export default App;
