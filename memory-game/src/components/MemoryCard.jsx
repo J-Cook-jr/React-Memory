37183b0b430e9db4a5f067ead4e65e9c061b1f7c
Jeff Cook <jecook2692@gmail.com>
2020-06-09 13:00:48 -0400

    adds DC logo on the back of memory card

--- memory-game/src/components/MemoryCard.jsx
+++ memory-game/src/components/MemoryCard.jsx
@@ -6,7 +6,7 @@ class MemoryCard extends Component {
     render() {
         return (
             <div className="MemoryCard">
-                <img src="" alt=""/>
+                <img src="https://www.digitalcrafts.com/img/logo-wrench-white.png" alt="img of wrench"></img>
             </div>
         );
     }
