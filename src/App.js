4c714afad69f6898296974257da1a3a6ddc4d167
Jeff Cook <jecook2692@gmail.com>
2020-06-11 12:44:10 -0400

    creates function to shuffle deck and calls generateDeck function

--- src/App.js
+++ src/App.js
@@ -11,14 +11,29 @@ function generateDeck() {
       isFlipped: false,
       symbol: symbols[i%8]
     })
-    
+    return shuffle(deck);
   }
+}
+
+/**
+ * Shuffles array in place.
+ * @param {Array} a items An array containing the items.
+ */
+function shuffle(a) {
+  var j, x, i;
+  for (i = a.length - 1; i > 0; i--) {
+      j = Math.floor(Math.random() * (i + 1));
+      x = a[i];
+      a[i] = a[j];
+      a[j] = x;
+  }
+  return a;
 }
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {deck: []};
+    this.state = {deck: generateDeck};
     this.state = {pickedCards: []};
   }
 
