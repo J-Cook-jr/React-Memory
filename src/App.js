4f85e22a6a2beb176900c80567d008fed023aebb
Jeff Cook <jecook2692@gmail.com>
2020-06-11 10:41:15 -0400

    changes App function to a class, creates a constructor and adds render to Apps.js file

--- src/App.js
+++ src/App.js
@@ -1,40 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import MemoryCard from './components/MemoryCard.jsx';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Memory Game</h1>
-        <h4>Match Cards to win</h4>
-      </header>
-      <div class="grid-container">
-        <MemoryCard></MemoryCard>
-        <MemoryCard></MemoryCard>
-        <MemoryCard></MemoryCard>
-        <MemoryCard></MemoryCard>
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Memory Game</h1>
+          <h4>Match Cards to win</h4>
+        </header>
+        <div class="grid-container">
+          <MemoryCard></MemoryCard>
+          <MemoryCard></MemoryCard>
+          <MemoryCard></MemoryCard>
+          <MemoryCard></MemoryCard>
+        </div>
+        <div class="grid-container">
+          <MemoryCard></MemoryCard>
+          <MemoryCard></MemoryCard>
+          <MemoryCard></MemoryCard>
+          <MemoryCard></MemoryCard>
+        </div>
+        <div class="grid-container">
+          <MemoryCard></MemoryCard>
+          <MemoryCard></MemoryCard>
+          <MemoryCard></MemoryCard>
+          <MemoryCard></MemoryCard>
+        </div>
+        <div class="grid-container">
+          <MemoryCard></MemoryCard>
+          <MemoryCard></MemoryCard>
+          <MemoryCard></MemoryCard>
+          <MemoryCard></MemoryCard>
+        </div>
       </div>
-      <div class="grid-container">
-        <MemoryCard></MemoryCard>
-        <MemoryCard></MemoryCard>
-        <MemoryCard></MemoryCard>
-        <MemoryCard></MemoryCard>
-      </div>
-      <div class="grid-container">
-        <MemoryCard></MemoryCard>
-        <MemoryCard></MemoryCard>
-        <MemoryCard></MemoryCard>
-        <MemoryCard></MemoryCard>
-      </div>
-      <div class="grid-container">
-        <MemoryCard></MemoryCard>
-        <MemoryCard></MemoryCard>
-        <MemoryCard></MemoryCard>
-        <MemoryCard></MemoryCard>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
